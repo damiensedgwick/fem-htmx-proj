ae9af2dc0b092da10813f2f8b9f49742952255ee
Damien Sedgwick <damienksedgwick@gmail.com>
2024-02-18 15:35:04 +0000

    done did it

--- cmd/main.go
+++ cmd/main.go
@@ -3,117 +3,169 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type Templates struct {
-	templates *template.Template
+type Template struct {
+	tmpl *template.Template
 }
 
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func newTemplate() *Templates {
-	return &Templates{
-		templates: template.Must(template.ParseGlob("views/*.html")),
+func newTemplate() *Template {
+	return &Template{
+		tmpl: template.Must(template.ParseGlob("views/*.html")),
 	}
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
 type Contact struct {
 	Name  string
 	Email string
+	Id    int
 }
 
-func newContact(name, email string) Contact {
+type Data struct {
+	Contacts []Contact
+}
+
+func NewData() *Data {
+	return &Data{
+		Contacts: []Contact{
+			{
+				Name:  "John Doe",
+				Email: "john.doe@gmail.com",
+				Id:    1,
+			},
+			{
+				Name:  "Jane Doe",
+				Email: "jain.doe@gmail.com",
+				Id:    2,
+			},
+		},
+	}
+}
+
+type FormData struct {
+	Errors map[string]string
+	Values map[string]string
+}
+
+func NewFormData() FormData {
+	return FormData{
+		Errors: map[string]string{},
+		Values: map[string]string{},
+	}
+}
+
+type PageData struct {
+	Data Data
+	Form FormData
+}
+
+func NewContact(id int, name, email string) Contact {
 	return Contact{
+		Id:    id,
 		Name:  name,
 		Email: email,
 	}
 }
 
-type Contacts = []Contact
-
-type Data struct {
-	Contacts Contacts
+func NewPageData(data Data, form FormData) PageData {
+	return PageData{
+		Data: data,
+		Form: form,
+	}
 }
 
-func (d *Data) hasEmail(email string) bool {
-	for _, contact := range d.Contacts {
-		if contact.Email == email {
+func contactExists(contacts []Contact, email string) bool {
+	for _, c := range contacts {
+		if c.Email == email {
 			return true
 		}
 	}
 	return false
 }
 
-func newData() Data {
-	return Data{
-		Contacts: []Contact{
-			newContact("John", "aoeu"),
-			newContact("Clara", "cd@gmail.com"),
-		},
-	}
-}
-
-type FormData struct {
-	Values map[string]string
-	Errors map[string]string
-}
-
-func newFormData() FormData {
-	return FormData{
-		Values: make(map[string]string),
-		Errors: make(map[string]string),
-	}
-}
-
-type Page struct {
-	Data Data
-	Form FormData
-}
-
-func newPage() Page {
-	return Page{
-		Data: newData(),
-		Form: newFormData(),
-	}
-}
-
 func main() {
 
 	e := echo.New()
-	e.Use(middleware.Logger())
 
-	page := newPage()
+	data := NewData()
+	id := 3
+
 	e.Renderer = newTemplate()
+	e.Use(middleware.Logger())
+	e.Static("/images", "images")
+	e.Static("/css", "css")
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", page)
+		return c.Render(200, "index.html", NewPageData(*data, NewFormData()))
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
 
-		if page.Data.hasEmail(email) {
-			formData := newFormData()
-			formData.Values["name"] = name
-			formData.Values["email"] = email
-			formData.Errors["email"] = "Email already exists"
+		if contactExists(data.Contacts, email) {
+			formData := FormData{
+				Errors: map[string]string{
+					"email": "Email already exists",
+				},
+				Values: map[string]string{
+					"name":  name,
+					"email": email,
+				},
+			}
 
-			return c.Render(422, "form", formData)
+			return c.Render(422, "contact-form", formData)
 		}
 
-		contact := newContact(name, email)
-		page.Data.Contacts = append(page.Data.Contacts, contact)
+		contact := NewContact(id, name, email)
+		id++
+		data.Contacts = append(data.Contacts, contact)
 
-		// TODO: ??????
-		c.Render(200, "form", newFormData())
+		formData := NewFormData()
+		err := c.Render(200, "contact-form", formData)
+
+		if err != nil {
+			return err
+		}
+
 		return c.Render(200, "oob-contact", contact)
 	})
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+
+		if err != nil {
+			return c.String(400, "Id must be an integer")
+		}
+
+		deleted := false
+		for i, contact := range data.Contacts {
+			if contact.Id == id {
+				data.Contacts = append(data.Contacts[:i], data.Contacts[i+1:]...)
+				deleted = true
+				break
+			}
+		}
+
+		if !deleted {
+			return c.String(400, "Contact not found")
+		}
+
+		time.Sleep(1 * time.Second)
+
+		return c.NoContent(200)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
 }
+
